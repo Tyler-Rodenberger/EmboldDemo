36f520298c990ca8a418be8add19e21c9385b6b6
Tyler Rodenberger <trodenbe@hamilton.edu>
2023-09-18 12:01:00 -0400

    all bad stuff

--- CPPShowcase.cpp
+++ CPPShowcase.cpp
@@ -4,3 +4,14 @@ int main() {
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
+
+
+/* This is a critical issue */
+// int divideByZero(int n) {
+//     int zero = 0;
+//     return n / zero;
+// }
+
+/* */
+
+
--- PythonShowcase.py
+++ PythonShowcase.py
@@ -1,7 +1,15 @@
+print("Hello, world!")
+
+
+
 def badLoop():
     for i in range(3):
         for i in range(2):
             print("too many i's")
+
+
+def badLoop():
+    return False
 
 badLoop()
 
