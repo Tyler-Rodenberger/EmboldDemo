653fbb34388ff510afb69d9a045310d787b5abb4
Tyler Rodenberger <trodenbe@hamilton.edu>
2023-09-20 16:32:16 -0400

    casued problems

--- PythonShowcase.py
+++ PythonShowcase.py
@@ -2,14 +2,14 @@ print("Hello, world!")
 
 
 
-# def badLoop():
-#     for i in range(3):
-#         for i in range(2):
-#             print("too many i's")
+def badLoop():
+    for i in range(3):
+        for i in range(2):
+            print("too many i's")
 
 
-# def badLoop():
-#     return False
+def badLoop():
+    return False
 
-# badLoop()
+badLoop()
 
